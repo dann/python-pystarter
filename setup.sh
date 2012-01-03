ea99a40de3bb1633b16cdadf0d3104ac3673b822
Dann <techmemo@gmail.com>
2012-01-03 20:20:17 +0900

    setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#==================================
+# Main
+#==================================
+PYTHON_MODULES=(
+jinja2
+yaml
+)
+
+setup() {
+    install_python_modules
+    install_template
+}
+
+install_template() {
+    mkdir -p ~/.pystarter
+    cp ./module_templates.yaml ~/.pystarter/
+}
+
+install_python_modules() {
+    for module in ${PYTHON_MODULES[@]}
+    do
+        pip install ${module}
+    done
+}
+
+setup
